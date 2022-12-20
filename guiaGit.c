cffe0d3338f8e243d155305871ed6d70bdcb553a
HoneyAnimeOtaku <hmaldonadod17@alumno.ipn.mx>
2022-12-20 17:11:53 -0600

    Prueba para usar git

---
+++ guiaGit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+printf("Terminologia \n\n");
+printf("Un REPOSITORIO es todo proyecto que esta siendo seguido por git,\n\n");
+printf("o sea que ya tiene un historial de cambios seguidos por git\n\n");
+printf("Un COMMIT es cada uno de los cambios registrados y subidos a git con mensajes\n\n");
+printf("Las RAMAS son ramificaciones, bifurcaciones que toma el proyecto\n");
+printf("La rama principal suele llamarse MASTER\n");
+printf("Cada que se quiere trabajar con una caracteristica nueva o corregir algo\n");
+printf("para trabajar en un ambiente aislado y si algo se rompe no se compromete\n");
+printf("el proyecto, luego esa rama se puede unificar con el proyecto principal\n");
+printf("Un CLONE es una copia exacta del repositorio. Clonar repositorio en su equipo\n");
+printf("local\n\n");
+printf("Un FORK es un proyecto diferente que se crea a partir de otro\n");
+printf("por ejemplo las distribuciones de Linux\n");
+printf("Git es un sistema de control de versiones VCS, eso es lo que es Git\n\n");
+printf("GitHub es una pagina para subir repositorios a la nube, que al\n");
+printf("final ese repositorio viene siendo una RAMA REMOTA llamada ORIGIN\n");
+
+
+return 0;
+}
